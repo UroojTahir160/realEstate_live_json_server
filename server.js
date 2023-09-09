2fb4c9c6cc2ccc35e50fc5a7a48fd5a89e969b1f
UroojTahir160 <uroojt113@gmail.com>
2023-09-09 14:18:44 +0500

    case insensitive search reverted

--- server.js
+++ server.js
@@ -8,33 +8,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-
-// Define a custom route for case-insensitive search
-server.get("/realEstateListing", (req, res) => {
-  const { address_like } = req.query;
-
-  if (!address_like) {
-    // If no search query is provided, return all data
-    res.jsonp(router.db.get("realEstateListing").value());
-  } else {
-    // Perform case-insensitive search on the 'title' field
-    const searchTerm = address_like.toLowerCase();
-    const results = router.db
-      .get("realEstateListing")
-      .filter((item) => item.address.toLowerCase().includes(searchTerm))
-      .value();
-
-    res.jsonp(results);
-  }
-});
-
 server.use(
  // Add custom route here if needed
  jsonServer.rewriter({
   "/*": "/$1",
  })
 );
-
 server.use(router);
 // Listen to port
 server.listen(3333, () => {
